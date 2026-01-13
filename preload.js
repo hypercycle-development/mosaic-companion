04d4caf71628d8134fbb94075004f36ad06c6b7d
NanezX <victor.nanezj@gmail.com>
2026-01-13 16:35:25 -0400

    preload the ipcs

--- preload.js
+++ preload.js
@@ -4,4 +4,6 @@ contextBridge.exposeInMainWorld("electronAPI", {
   logInput: (text) => ipcRenderer.invoke("log-input", text),
   getCsvPath: () => ipcRenderer.invoke("get-csv-path"),
   checkForUpdates: () => ipcRenderer.invoke("check-for-updates"),
+  getUpdateSettings: () => ipcRenderer.invoke("get-update-settings"),
+  setUpdateSettings: (settings) => ipcRenderer.invoke("set-update-settings", settings),
 });
