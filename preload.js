a5281778924e332a8ddda14e3b9f80299126f960
NanezX <victor.nanezj@gmail.com>
2026-01-13 12:14:20 -0400

    add ipc preload for check updates

--- preload.js
+++ preload.js
@@ -3,4 +3,5 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
   logInput: (text) => ipcRenderer.invoke("log-input", text),
   getCsvPath: () => ipcRenderer.invoke("get-csv-path"),
+  checkForUpdates: () => ipcRenderer.invoke("check-for-updates"),
 });
