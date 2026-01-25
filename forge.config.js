03279f8a36e5203723a9c5902e3190718a20a0ba
NanezX <victor.nanezj@gmail.com>
2026-01-24 22:02:45 -0400

    fix naming

--- forge.config.js
+++ forge.config.js
@@ -72,6 +72,8 @@ export default {
       name: '@electron-forge/maker-deb',
       platforms: ['linux'],
       config: {
+        name: 'mosaic-companion',
+        productName: 'Mosaic Companion',
         options: {
           maintainer: 'hern@hypercycle.ai',
           homepage: 'https://hypercycle.ai',
