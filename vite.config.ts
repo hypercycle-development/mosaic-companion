6d8f2ae1ca075f51abe03df4bf39000f90c9b307
NanezX <victor.nanezj@gmail.com>
2026-02-12 12:12:02 -0400

    vite read the resolving from path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './', // Ensure relative paths for Electron file:// protocol
+  resolve: {
+    alias: {
+      '@plugins': path.resolve(__dirname, 'plugins'),
+      '@': path.resolve(__dirname),
+    },
+  },
   build: {
     outDir: 'dist/renderer',
     emptyOutDir: true,
