deeb8aee9ce1956ff28a3a85262bffd5c808e3e6
NanezX <victor.nanezj@gmail.com>
2026-02-16 17:29:21 -0400

    chore: Remove ZIP archives from build and release artifacts for Linux and macOS.

--- forge.config.js
+++ forge.config.js
@@ -136,10 +136,6 @@ export default {
                 }
             }
         }
-        // {
-        //     name: '@electron-forge/maker-zip',
-        //     platforms: ['linux', 'darwin', 'win32']
-        // }
     ],
 
     publishers: [
